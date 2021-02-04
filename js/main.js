1f24c50fb722331d9e4bc089f09b765a954a424f
Alina <alinka2802karamel78@gmail.com>
2021-02-04 22:03:08 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+
+    })
+}
+
+var btn = document.querySelector ('.btn')
+var info =document.querySelector ('#info')
+
+btn.addEventListener ('click',() => {
+    scrollTo(info);
+})
+
+$(function () {
+   $(window).scroll(function() {
+        $(' #learn .section-title').each(function(){
+          var imagePos = $(this).offset().top;
+
+          var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) {
+            $(this).addClass("fadeInUp");
+        }
+    });
+  });
+})
+
+
+$(function () {
+    $(window).scroll(function() {
+         $('.dream-1').each(function(){
+           var imagePos = $(this).offset().top;
+ 
+           var topOfWindow = $(window).scrollTop();
+         if (imagePos < topOfWindow+650) {
+             $(this).addClass("fadeInUp");
+         }
+     });
+   });
+ })
+
+ 
+ $(function () {
+    $(window).scroll(function() {
+         $('.dream-2').each(function(){
+           var imagePos = $(this).offset().top;
+ 
+           var topOfWindow = $(window).scrollTop();
+         if (imagePos < topOfWindow+650) {
+             $(this).addClass("fadeInUp");
+         }
+     });
+   });
+ })
+
+ $(function () {
+    $(window).scroll(function() {
+         $('.dream-3').each(function(){
+           var imagePos = $(this).offset().top;
+ 
+           var topOfWindow = $(window).scrollTop();
+         if (imagePos < topOfWindow+650) {
+             $(this).addClass("fadeInUp");
+         }
+     });
+   });
+ })
+
+ $(function () {
+    $(window).scroll(function() {
+         $('#info .section-title').each(function(){
+           var imagePos = $(this).offset().top;
+ 
+           var topOfWindow = $(window).scrollTop();
+         if (imagePos < topOfWindow+650) {
+             $(this).addClass("fadeInUp");
+         }
+     });
+   });
+ })
+
+ $(function () {
+    $(window).scroll(function() {
+         $('.form').each(function(){
+           var imagePos = $(this).offset().top;
+ 
+           var topOfWindow = $(window).scrollTop();
+         if (imagePos < topOfWindow+650) {
+             $(this).addClass("rotateInUpLeft");
+         }
+     });
+   });
+ })
+
+
+
+ 
+
